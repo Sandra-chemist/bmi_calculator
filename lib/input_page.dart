6451923517be58d3d604817c486d2930a17d0b60
Sandra Skibiszewska <sandra.skibiszewska.programista@gmail.com>
2023-03-21 15:43:37 +0100

    Slider widget was added

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -16,6 +16,7 @@ class InputPage extends StatefulWidget {
 
 class _InputPageState extends State<InputPage> {
   Gender selectedGender = Gender.male;
+  int height = 180;
 
   @override
   Widget build(BuildContext context) {
@@ -76,7 +77,7 @@ class _InputPageState extends State<InputPage> {
                       textBaseline: TextBaseline.alphabetic,
                       children: [
                         Text(
-                          '180',
+                          height.toString(),
                           style: kNumberTextStyle,
                         ),
                         Text(
@@ -84,7 +85,18 @@ class _InputPageState extends State<InputPage> {
                           style: kLabelTextStyle,
                         ),
                       ],
-                    )
+                    ),
+                    Slider(
+                        value: height.toDouble(),
+                        min: 120.0,
+                        max: 200.0,
+                        activeColor: Color(0xFFEB1555),
+                        inactiveColor: Color(0xFF8D8E98),
+                        onChanged: (double newValue) {
+                          setState(() {
+                            height = newValue.round();
+                          });
+                        }),
                   ],
                 ),
                 onPress: () {},
