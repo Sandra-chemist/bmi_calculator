d02583f7ffda8487cb5eb48acb187110478aedd1
Sandra Skibiszewska <sandra.skibiszewska.programista@gmail.com>
2023-03-22 10:38:47 +0100

    a new dart file was created

---
+++ lib/bottom_button.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'constants.dart';
+
+class BottomButton extends StatelessWidget {
+  BottomButton({required this.onTap, required this.buttonTittle});
+
+  final Function() onTap;
+  final String buttonTittle;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        child: Center(
+          child: Text(
+            buttonTittle,
+            style: kLargeButtonTextStyle,
+          ),
+        ),
+        color: kBottomContainerColour,
+        margin: EdgeInsets.only(top: 5.0),
+        width: double.infinity,
+        height: kBottomContainerHeight,
+      ),
+    );
+  }
+}
